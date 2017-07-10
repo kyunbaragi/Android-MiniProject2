0bbfd4d5b1b550e83225a8c34a14b7ee0b226fba
Yunkyun Han <kyunbaragi@daum.net>
2017-07-10 20:59:55 +0900

    Start MiniProject2

---
+++ app/src/main/java/com/project2/mini/yunkyun/miniproject2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.project2.mini.yunkyun.miniproject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
